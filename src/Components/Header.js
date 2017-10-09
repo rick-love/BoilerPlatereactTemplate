ddc1db8d6272d2f0bf73f2a932254166e5ac82d2
Richard Love <ricklove@outloook.com>
2017-10-09 15:06:49 -0400

    added a modal

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// stateless components don't use (this)
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Indecision"
+}
+
+export default Header;

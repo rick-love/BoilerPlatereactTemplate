9866424d452324e84822961cf53539d55a1f6056
Richard Love <ricklove@outloook.com>
2017-10-11 13:59:28 -0400

    this is my boiler plate react template

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './Components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './Styles/styles.scss'
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));

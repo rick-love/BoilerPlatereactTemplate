ead77198cec9701007ca6e9e07dbaa6a6a4217a2
Richard Love <ricklove@outloook.com>
2017-10-11 15:56:45 -0400

    fixed css issue

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import './Styles/styles.scss'
+import './styles/styles.scss'
 
 ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
